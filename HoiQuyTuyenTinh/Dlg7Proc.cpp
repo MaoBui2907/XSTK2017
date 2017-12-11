d10725b7ae93fc2eb40514f3fd2fe2a8f84f0ec6
Nguyenmao <maonguyen199873@gmail.com>
2017-12-11 21:59:39 +0700

    update 11/12

---
+++ HoiQuyTuyenTinh/Dlg7Proc.cpp
@@ -0,0 +1,33 @@
+﻿#include "HoiQuyTuyenTinh.h"
+#include "resource.h"
+BOOL CALLBACK Dlg7Proc(HWND wd, UINT id, WPARAM w, LPARAM l)
+{
+	switch (id)
+	{
+	case WM_INITDIALOG:
+	{
+		ComboBox_AddString(GetDlgItem(wd, IDC_COMBO1), L"Trung bình của tồng thể");
+		ComboBox_AddString(GetDlgItem(wd, IDC_COMBO1), L"Tỉ lệ của tổng thể");
+		ComboBox_AddString(GetDlgItem(wd, IDC_COMBO1), L"Phương sai của tổng thể");
+		ComboBox_AddString(GetDlgItem(wd, IDC_COMBO1), L"Hệ số a của đường hồi quy");
+		return 0;
+	}
+	case WM_PAINT :
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(wd, &ps);
+		
+		EndPaint(wd, &ps);
+		return 0;
+	}
+	case WM_COMMAND:
+	{
+		if (LOWORD(w) == IDC_BUTTON1)
+		{
+			EndDialog(wd, 0);
+		}
+		return 0;
+	}
+	}
+	return 0;
+}
